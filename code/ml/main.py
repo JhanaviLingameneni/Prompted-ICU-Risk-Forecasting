588e5e39205727f4ed570c82bc07e0bc90cc2338
Jean Luis Urena <jlurena@teladochealth.com>
2026-04-04 11:56:16 -0400

    Fix small usage issue

--- code/ml/main.py
+++ code/ml/main.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     from joblib import dump
     from train import ann, scaler
     # ANN is a Keras model, so we save it in a Keras-compatible format.
-    ann().save("../models/ann_model.keras")
+    ann().model.save("../models/ann_model.keras")
     # Save the scaler using joblib
     dump(scaler, "../models/ann_scaler.bin")
